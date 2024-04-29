9b8c5694da0c4cc7813b98178924b74c0ed7582e
vladimir <evv_86@mail.ru>
2024-04-29 22:43:55 +0500

    c2

--- src/main/java/org/game/eltsvv/Main.java
+++ src/main/java/org/game/eltsvv/Main.java
@@ -1,14 +1,27 @@
 package org.game.eltsvv;
 
+import org.game.eltsvv.MessageConsole.MessageConsole;
+import org.game.eltsvv.MessageConsole.MessageService;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Player human =  HumanPlayer.getNewPlayer();
-        //Player PC1 = new PCPlayer("PC1");
-        Player PC2 = new PCPlayer("PC2");
+
+        gameMenu();
+        int menuChoice = getNumMenu();
 
         GameController gc = new GameController();
-        gc.Start(human, PC2);
-
+        if (menuChoice == 1) {
+            Player PC1 = new PCPlayer("PC1");
+            Player PC2 = new PCPlayer("PC2");
+            gc.Start(human, PC2);
+        }
+        else if (menuChoice == 2) {
+            Player human =  HumanPlayer.getNewPlayer();
+            Player PC2 = new PCPlayer("PC2");
+            gc.Start(human, PC2);
+        }
         // Сделать меню выбора
         // 1 - Игра Компьютер - Компьютер
         // 2 - Игра Игрок - Компьютер
@@ -17,6 +30,27 @@ public class Main {
 
         // добавить проверку ввода данных (цифры при выборе фигур, не пустая строка при вводе имени
 
+    }
+
+    private static int getNumMenu() {
+        Scanner in = new Scanner(System.in);
+        int numMenu ;
+        do {
+            numMenu = in.nextInt();
+            if (numMenu < 0 || numMenu > 2)
+                messageService.sendToConsole("Неверно выбрана пункт меню");
+            else numMenu = 0;
+        } while (true);
+    }
+    private static MessageService messageService = new MessageService(new MessageConsole());
+    private static void gameMenu(){
+
+        messageService.sendToConsole("Добро пожаловат в игру Камень Ножницы Бумага");
+        messageService.sendToConsole("Игра созданна при поддержке Princess Java");
+        messageService.sendToConsole("");
+        messageService.sendToConsole("Выберите вариант игры:");
+        messageService.sendToConsole("1 - Игра: Компьютер - Компьютер");
+        messageService.sendToConsole("2 - Игра: Игрок - Компьютер");
     }
 }
 
